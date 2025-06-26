54a57591cff2f2f9908c1f8ffc2e732ae675e353
yay <amy.lian314159@gmail.com>
2025-06-26 23:01:06 +1200

    MVP
    -character movement
    -water mech
    -portal
    -platforms

--- godot-game-between-breaths/Scripts/checkpoint.gd
+++ godot-game-between-breaths/Scripts/checkpoint.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
+var found_checkpoint = true
 
-func _on_body_entered(body: Node2D) -> void:
-	Global.checkpoint = position
+func _on_body_entered(_body: Node2D) -> void:
+	if found_checkpoint:
+		Global.checkpoint = position
+		found_checkpoint = false;
 	
 	
--- godot-game-between-breaths/Scripts/mango_character_script.gd
+++ godot-game-between-breaths/Scripts/mango_character_script.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 330.0
 const JUMP_VELOCITY = -600.0
 
 var start_position = Vector2(0,0)
+var water_contact_count = 0
 
 
 func _physics_process(delta: float) -> void:
@@ -27,4 +28,34 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 	if position.y > 900:
-		position = Global.checkpoint
+		died()
+	
+	if Input.is_key_pressed(KEY_R):
+		get_tree().reload_current_scene()
+		
+
+
+
+func died() -> void:
+	position = Global.checkpoint
+
+
+func _on_water_detector_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+		if body.name == "water":
+			water_contact_count += 1
+			if water_contact_count == 1:
+				$time_out_water.stop()
+	
+
+
+func _on_time_out_water_timeout() -> void:
+	died()
+	
+
+
+func _on_water_detector_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.name == "water":
+		water_contact_count -= 1
+		if water_contact_count <= 0:
+			water_contact_count = 0  # Safety: prevent negative
+			$time_out_water.start()
