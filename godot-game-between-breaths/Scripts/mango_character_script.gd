cc6344ab1cd18f7086653e4a8cf7d9cc3940ff8f
yay <amy.lian314159@gmail.com>
2025-07-12 23:58:11 +1200

    added more code and variables for character movement

--- godot-game-between-breaths/Scripts/mango_character_script.gd
+++ godot-game-between-breaths/Scripts/mango_character_script.gd
@@ -1,7 +1,23 @@
 extends CharacterBody2D
 
+enum STATES{MOVE, CLIMB}
 
-const SPEED = 330.0
+#exports of movement values
+@export var max_speed: = 350
+@export var acceleration: = 1000
+@export var air_acceleration: = 1500
+@export var friction: = 1000
+@export var air_friction: = 500
+@export var up_gravity: = 1400
+@export var down_gravity: = 1400
+@export var jump_amount: = -700
+
+@export var state := STATES.MOVE
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+const SPEED = 350.0
 const JUMP_VELOCITY = -600.0
 
 var start_position = Vector2(0,0)
@@ -9,23 +25,49 @@ var water_contact_count = 0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
+	match state:
+		STATES.MOVE:
+			# Add the gravity.
+			apply_gravity(delta)
+			
+			#if not is_on_floor():
+				#velocity.y += up_gravity * delta
+
+			# Handle jump.
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				velocity.y = jump_amount
+
+			# Get the input direction and handle the movement/deceleration.
+			# As good practice, you should replace UI actions with custom gameplay actions.
+			var direction := Input.get_axis("move_left", "move_right")
+				
+			if direction == 0:
+				apply_friction(delta)
+				animation_player.play("idle")
+			if direction != 0:
+				accelerate_x(direction, delta)
+				animation_player.play("run")
+
+			move_and_slide()
+	
+func accelerate_x(horizontal_direction: float, delta: float) -> void:
+	var acceleration_amount = acceleration
+	if not is_on_floor(): acceleration_amount = air_acceleration
+	velocity.x = move_toward(velocity.x, max_speed * horizontal_direction, acceleration_amount * delta)
+
+func apply_friction(delta: float) -> void:
+	var friction_amount = friction
+	if not is_on_floor(): friction_amount = air_friction
+	velocity.x = move_toward(velocity.x, 0.0, friction_amount * delta)
+
+func apply_gravity(delta: float) -> void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		if velocity.y <= 0:
+			velocity.y += up_gravity * delta
+		else:
+			velocity.y += down_gravity * delta
+	pass
 	
 	
 
@@ -42,7 +84,7 @@ func died() -> void:
 	position = Global.checkpoint
 
 
-func _on_water_detector_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+func _on_water_detector_body_shape_entered(_body_rid: RID, body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
 		if body.name == "water":
 			water_contact_count += 1
 			if water_contact_count == 1:
@@ -55,7 +97,7 @@ func _on_time_out_water_timeout() -> void:
 	
 
 
-func _on_water_detector_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+func _on_water_detector_body_shape_exited(_body_rid: RID, body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
 	if body.name == "water":
 		water_contact_count -= 1
 		if water_contact_count <= 0:
